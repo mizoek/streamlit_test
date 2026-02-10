ff8bbba963b3a1b3720f4071f5948fe2bd7dc654
mizoek <mizoek.touwa@gmail.com>
2026-02-10 16:59:41 +0900

    レシート帳簿アプリwebアプリ化その１

--- run_app.bat
+++ run_app.bat
@@ -1,3 +1,20 @@
+@echo off
 cd /d %~dp0
-streamlit run app_ver3.py
+
+:: 8501番ポートが使用中かを確認
+netstat -ano | findstr :8501 | findstr LISTENING > nul
+
+if %errorlevel% equ 0 (
+  echo --------------------------------------------------
+  echo レシート帳簿アプリは既に起動しているようです。
+  echo ブラウザのタブを確認してください。
+  echo --------------------------------------------------
+  pause
+  exit
+)
+
+:: 起動処理
+echo アプリの起動中…
+streamlit run app_ver3.py --server.port 8501
+
 pause
